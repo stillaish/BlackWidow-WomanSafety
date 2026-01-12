ea0788c58cfeb2e7083813d7eb45c9fabd1a0dcb
Aish Maheshwari <155080572+stillaish@users.noreply.github.com>
2026-01-12 22:27:42 +0530

    Update Firebase API key and identifiers

--- build/firebase-messaging-sw.js
+++ build/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD1W8JjXOOMq5oi3dp2Xhu1i4WZHL8RiqI",
+  apiKey: "AIzaSyD1W8JjXOOMq5oi3dp2Xxxxxxxxxx",
   authDomain: "black-widow-rand.firebaseapp.com",
   projectId: "black-widow-rand",
   storageBucket: "black-widow-rand.firebasestorage.app",
-  messagingSenderId: "269000336764",
-  appId: "1:269000336764:web:b53c2bb13c8df857faf0f9"
+  messagingSenderId: "2690003xxxxxx",
+  appId: "1:269000336764:web:b53c2bb13cxxxxxxxxx"
 });
 
 const messaging = firebase.messaging();
